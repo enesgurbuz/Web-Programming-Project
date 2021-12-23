7cfa7ab4123b8cad3e2a3060aa4629a1acffc0f9
Zehra Merve Gül <zehramervegul@gmail.com>
2021-12-23 13:48:21 +0300

    Model added

---
+++ Models/EvrakTipi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EvrakYonetimSistemi.Models
+{
+    public class EvrakTipi
+    {
+        [Key]
+        public int TipID { get; set; }
+        public string TipAdi { get; set; }
+
+        public IList<Evrak> Evraks { get; set; }
+    }
+}
